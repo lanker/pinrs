9373bc85ab6e42535c4ed91553ed5e837f781da9
Fredrik Lanker <fredrik@lanker.se>
2025-03-07 12:44:37 +0100

    Add license

--- src/api/handlers/tags.rs
+++ src/api/handlers/tags.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Fredrik Lanker <fredrik@lanker.se>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use crate::{AppState, PostID, TagID};
 use axum::extract::State;
 use axum::routing::get;
